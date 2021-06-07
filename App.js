3f456508a368d4ebd08cef4372405bbdd093cad4
wafachaari <wafachaari30@gmail.com>
2021-06-07 16:25:32 +0200

    updated

--- App.js
+++ App.js
@@ -1,30 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+// import react native gesture handler
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
 import Start from './components/Start';
 import Chat from './components/Chat';
-
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
-
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+ 
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start"  >
+          <Stack.Screen name="Start" component={Start}   options={{ headerShown: false }} />
+          <Stack.Screen name="Chat" component={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-export default App;
